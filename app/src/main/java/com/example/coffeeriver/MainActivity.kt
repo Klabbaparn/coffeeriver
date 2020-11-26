24a8837ea4928e599adb5ba8f3afece3be027f97
Hugo Nolte <hugonolte@hotmail.se>
2020-11-26 12:05:54 +0100

    Layout and Activities Defined

--- app/src/main/java/com/example/coffeeriver/MainActivity.kt
+++ app/src/main/java/com/example/coffeeriver/MainActivity.kt
@@ -1,11 +1,17 @@
 package com.example.coffeeriver
 
-import androidx.appcompat.app.AppCompatActivity
+import android.graphics.Typeface
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        try {
+            this.supportActionBar!!.hide()
+        } catch (e: NullPointerException) {
+        }
+
         setContentView(R.layout.activity_main)
     }
 }
